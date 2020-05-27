ac899fe39ff8a3e922aec4fd2e0d2d6a414f22d4
code cheng <13275768320@163.com>
2020-05-27 11:50:10 +0800

    Create GuavaCacheConfigure.java
    add GuavaCacheConfigure

---
+++ src/main/java/com/ducheng/springboot/aotoconfigure/GuavaCacheConfigure.java
@@ -0,0 +1,38 @@
+package com.ducheng.springboot.aotoconfigure;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.ducheng.springboot.web.RetuenCaptchReturnValueHandler;
+import com.ducheng.springboot.webmvcconfigure.CaptchaWebMvcConfiguation;
+import com.ducheng.springboot.webmvcconfigure.GuavaCacheWebMvcConfiguation;
+
+
+@Configuration
+@EnableConfigurationProperties(GuavaCacheConfigure.class)
+@ConfigurationProperties("com.ducheng.guava.cache")
+public class GuavaCacheConfigure {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(RetuenCaptchReturnValueHandler.class);
+	
+	private boolean enable = true;
+
+	public boolean isEnable() {
+		return enable;
+	}
+	public void setEnable(boolean enable) {
+		this.enable = enable;
+	}
+	@Bean
+	public WebMvcConfigurer excelWebMvcConfigurer() {
+		GuavaCacheWebMvcConfiguation configurer = new GuavaCacheWebMvcConfiguation();
+		configurer.setEnable(this.enable);
+		LOGGER.info("初始化 guavacache 容器");
+		return configurer;
+	}
+}
